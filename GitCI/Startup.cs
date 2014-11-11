983f4d547b7b2782c9956997375c9218fceb0beb
ToanNN <sr.nguyenngoctoan@gmail.com>
2014-11-12 09:43:52 +1000

    InitialProjectSetup

---
+++ GitCI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitCI.Startup))]
+namespace GitCI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
